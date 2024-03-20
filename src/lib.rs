c423ce15f68efbde25a8b693d171a56f05e2bf59
Pyxrs <pyxrss@gmail.com>
2024-03-20 12:02:12 -0600

    replace dashmap

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 pub use anyhow;
-pub use dashmap as dash;
 pub use flume as mpmc;
 pub use futures_lite as futures;
 pub use log;
 pub use parking_lot as sync;
 pub use rand;
 pub use rayon;
+pub use scc;
 
 pub mod prelude {
     pub use anyhow::*;
