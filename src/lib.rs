76486a685b5cba8f83c71f5f12bc4576cf33c1b0
Pyxrs <pyxrss@gmail.com>
2024-04-30 21:09:59 -0600

    closure macro to add a missing rust feature

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@ pub use scc;
 pub mod prelude {
     pub use anyhow;
     pub use chrono::prelude as time;
+    pub use closure::closure;
     pub use futures::{
         executor::block_on, join, pending, pin_mut, poll, ready, select, select_biased,
         stream_select, try_join,
