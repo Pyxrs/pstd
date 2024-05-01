39db8c7fe481254a8d0f70983ee7b9a4481e6fc2
Pyxrs <pyxrss@gmail.com>
2024-04-30 20:41:36 -0600

    futures macros

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 pub use anyhow;
 pub use chrono as time;
 pub use flume as mpmc;
-pub use futures_lite as futures;
+pub use futures;
 pub use log;
 pub use parking_lot as sync;
 pub use rand;
@@ -10,7 +10,10 @@ pub use scc;
 
 pub mod prelude {
     pub use anyhow;
-    pub use chrono::prelude::*;
-    pub use futures_lite::future::block_on;
+    pub use chrono::prelude as time;
+    pub use futures::{
+        executor::block_on, join, pending, pin_mut, poll, ready, select, select_biased,
+        stream_select, try_join,
+    };
     pub use log::{debug, error, info, trace, warn};
 }
