8359aa0400ac6a788af084c122fbdc24de669aa0
Pyxrs <pyxrss@gmail.com>
2024-07-31 17:21:18 -0700

    unify hash naming conventions

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-pub use ahash;
 pub use chrono as time;
 pub use fastrand;
 pub use flume as mpmc;
@@ -8,13 +7,26 @@ pub use parking_lot as sync;
 pub use rand;
 pub use rayon;
 pub use relative_path as path;
-pub use scc as chash; // Concurrent hash types
 
 pub mod prelude {
     pub use chrono::prelude as time;
     pub use closure::closure;
     pub use log::{debug, error, info, trace, warn};
     pub use std::time::{Duration, Instant};
+}
+
+// Synchronous insecure hash types
+pub mod ahash {
+    pub use ahash::{AHashMap, AHashSet, AHasher};
+}
+
+// Concurrent hash types
+pub mod chash {
+    pub use scc::{
+        Bag as CBag, HashCache as CHashCache, HashIndex as CHashIndex, HashMap as CHashMap,
+        HashSet as CHashSet, LinkedEntry as CLinkedEntry, LinkedList as CLinkedList,
+        Queue as CQueue, Stack as CStack, TreeIndex as CTreeIndex,
+    };
 }
 
 pub mod anyhow {
