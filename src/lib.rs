fe12ac64268c0602069805b6823cf3dfd73e5031
Pyxrs <pyxrss@gmail.com>
2024-11-01 22:45:26 -0600

    Utility function for games

--- src/lib.rs
+++ src/lib.rs
@@ -75,3 +75,45 @@ impl<T: PartialOrd> ClampRef for T {
         clamp_ref(self, min, max);
     }
 }
+
+#[cfg(feature = "game")]
+pub mod game {
+    use std::f32::consts::PI;
+
+    pub fn sun_direction(time_of_day: f32, latitude: f32, time_of_year: f32) -> (f32, f32, f32) {
+        // Convert time of day from [0, 1] to [-π, π]
+        let solar_time_rads = 2.0 * PI * (time_of_day - 0.5);
+
+        // Convert time of year from [0, 1] to [-0.41, 0.41]
+        // offset by 0.25 so declination is 0 at equinoxes
+        let declination_rads = (2.0 * PI * (time_of_year - 0.25)).sin() * 23.45f32.to_radians();
+
+        // Convert latitude from [0, 1] to [-π/2, π/2]
+        let latitude_rads = PI * (latitude - 0.5);
+
+        // equations adapted from https://stackoverflow.com/questions/8708048/position-of-the-sun-given-time-of-day-latitude-and-longitude
+        let zenith_rads = (latitude_rads.sin() * declination_rads.sin()
+            + latitude_rads.cos() * declination_rads.cos() * solar_time_rads.cos())
+        .acos();
+
+        let mut azimuth_rads = ((latitude_rads.sin() * zenith_rads.cos() - declination_rads.sin())
+            / (latitude_rads.cos() * zenith_rads.sin()))
+        .acos();
+
+        let elevation_rads = (declination_rads.sin() * latitude_rads.sin()
+            + declination_rads.cos() * latitude_rads.cos() * solar_time_rads.cos())
+        .asin();
+
+        if solar_time_rads > 0.0f32 {
+            azimuth_rads += PI;
+        } else {
+            azimuth_rads = 3.0 * PI - azimuth_rads;
+        }
+
+        let x = azimuth_rads.sin() * elevation_rads.cos();
+        let y = elevation_rads.sin();
+        let z = azimuth_rads.cos() * elevation_rads.cos();
+
+        (x, y, z)
+    }
+}
