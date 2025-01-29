16411faf7606f9df7f657f1d9a1b915c5fcaaeb1
Pyxrs <pyxrss@gmail.com>
2025-01-29 16:20:58 -0700

    0.2

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,18 @@
 pub use chrono as time;
+pub use closure;
+#[cfg(feature = "rand")]
 pub use fastrand;
-pub use flume as mpmc;
-pub use futures;
+#[cfg(feature = "log")]
 pub use log;
-pub use parking_lot as sync;
+#[cfg(feature = "rand")]
 pub use rand;
 pub use rayon;
 pub use relative_path as path;
 
 pub mod prelude {
-    pub use chrono::prelude as time;
     pub use closure::closure;
     pub use log::{debug, error, info, trace, warn};
     pub use std::time::{Duration, Instant};
-}
-
-/// Synchronous insecure hash types
-pub mod ahash {
-    pub use ahash::{AHashMap, AHashSet, AHasher};
-}
-
-/// Concurrent hash types
-pub mod chash {
-    pub use scc::{
-        Bag as CBag, HashCache as CHashCache, HashIndex as CHashIndex, HashMap as CHashMap,
-        HashSet as CHashSet, LinkedEntry as CLinkedEntry, LinkedList as CLinkedList,
-        Queue as CQueue, Stack as CStack, TreeIndex as CTreeIndex,
-    };
 }
 
 pub mod anyhow {
@@ -34,9 +20,23 @@ pub mod anyhow {
     pub type AnyResult<T> = anyhow::Result<T>;
 }
 
-pub mod asyn {
+#[cfg(feature = "async")]
+pub mod r#async {
     pub use futures::*;
-    pub use futures::{executor::block_on, future::*};
+}
+
+pub mod collections {
+    pub use ahash as insecure;
+}
+
+/// Concurrency
+pub mod sync {
+    // Channels
+    pub use flume::*;
+    // Locks
+    pub use parking_lot::*;
+    // Collections
+    pub use scc::*;
 }
 
 /// Returns input value clamped to the interval `[min, max]`.
@@ -55,7 +55,7 @@ pub fn clamp<T: PartialOrd>(val: T, min: T, max: T) -> T {
 }
 
 /// Returns input value clamped to the interval `[min, max]`.
-//#[inline]
+#[inline]
 pub fn clamp_ref<T: PartialOrd>(val: &mut T, min: T, max: T) {
     if *val > min {
         if *val > max {
@@ -78,9 +78,12 @@ impl<T: PartialOrd> ClampRef for T {
 
 #[cfg(feature = "game")]
 pub mod game {
-    use std::f32::consts::PI;
+    pub use glam;
+    pub use noise;
 
-    pub fn sun_direction(time_of_day: f32, latitude: f32, time_of_year: f32) -> (f32, f32, f32) {
+    pub fn sun_direction(time_of_day: f32, latitude: f32, time_of_year: f32) -> glam::Vec3A {
+        use std::f32::consts::PI;
+
         // Convert time of day from [0, 1] to [-π, π]
         let solar_time_rads = 2.0 * PI * (time_of_day - 0.5);
 
@@ -110,10 +113,10 @@ pub mod game {
             azimuth_rads = 3.0 * PI - azimuth_rads;
         }
 
-        let x = azimuth_rads.sin() * elevation_rads.cos();
-        let y = elevation_rads.sin();
-        let z = azimuth_rads.cos() * elevation_rads.cos();
-
-        (x, y, z)
+        glam::Vec3A::new(
+            azimuth_rads.sin() * elevation_rads.cos(),
+            elevation_rads.sin(),
+            azimuth_rads.cos() * elevation_rads.cos(),
+        )
     }
 }
